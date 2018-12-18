b4b851da281e8d754cf5c9c755e42348e3ced6c5
James P. Fulford <james.patrick.fulford@gmail.com>
2018-12-18 12:38:53 -0500

    Adding shared styles constants

---
+++ src/styles.js
@@ -0,0 +1,13 @@
+export const colors = {
+	background: '#3D2F40',
+	textMain: '#F4BDFF',
+	textSubtle: '#7A5F7F',
+};
+export const fonts = {
+	primary: 'FredokaOne',
+};
+export const size = {
+	title: 80,
+	secondary: 22,
+	subtitle: 18,
+};
