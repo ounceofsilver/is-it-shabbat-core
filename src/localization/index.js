57004aca89943dfe053160bb2cccd1cbb5a8521c
James Fulford <james.fulford@outlook.com>
2019-04-01 00:22:18 -0400

    feat(localization): Using i18n-js

--- src/localization/index.js
+++ src/localization/index.js
@@ -1,6 +1,8 @@
+// eslint-disable-next-line import/no-unresolved
+import i18n from 'i18n-js'; // is peer dependency
+
 import en from './en';
 
-// resources to i18n
-export default {
-	en,
-};
+
+i18n.fallbacks = true;
+i18n.translations = { en };
